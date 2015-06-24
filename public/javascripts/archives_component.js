9a1cfb7551a438721209595097137f337cac4be3
Xu Hu <hxtheone@gmail.com>
2015-06-25 00:12:38 +0800

    filter articles by tag

--- public/javascripts/archives_component.js
+++ public/javascripts/archives_component.js
@@ -1,3 +1,37 @@
+var TagList = React.createClass({
+  render: function () {
+    var tags = [], tagBtns = [];
+    var $this = this;
+    this.props.data.forEach(function (article) {
+      article.split('*')[3].split('.')[0].split('-').forEach(function (tag) {
+        if (tags.indexOf(tag) === -1){
+          tags.push(tag);
+          tagBtns.push(<button onClick={$this.filterArticle.bind(this, tag)}>{tag}</button>);
+        }
+      });
+    });
+    return (
+      <div className="widget tag-widget">
+        {tagBtns}<br />
+        <button onClick={this.cancelFilter}>Cancel</button>
+      </div>
+    );
+  },
+  filterArticle: function (tag) {
+    var parent = this.props.parent;
+    var srcArr = parent.state.filteredArticles;
+    var distArr = [];
+    srcArr.forEach(function (article) {
+      if (article.indexOf(tag) !== -1)
+        distArr.push(article);
+    });
+    parent.setState({filteredArticles: distArr});
+  },
+  cancelFilter: function () {
+    this.props.parent.setState({filteredArticles: this.props.data});
+  }
+});
+
 var ArticleList = React.createClass({
   render: function () {
     var articleLinks = this.props.data.map(function (article) {
@@ -17,18 +51,19 @@ var ArticleList = React.createClass({
 
 var Archives = React.createClass({
   getInitialState: function() {
-    return {articles: []};
+    return {articles: [], filteredArticles: []};
   },
   loadArticles: function () {
     $.get('/all_articles', function (data) {
-      this.setState({articles: data.articles});
+      this.setState({articles: data.articles, filteredArticles: data.articles});
     }.bind(this));
   },
   render: function () {
     return (
       <div>
         <h1>Archives</h1>
-        <ArticleList data={this.state.articles}/>
+        <TagList data={this.state.articles} parent={this}/>
+        <ArticleList data={this.state.filteredArticles}/>
       </div>
     );
   },
